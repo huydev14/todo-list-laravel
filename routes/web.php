20cc5a50672099daaa61a8285c4ffd1ebf7ae7a9
trgiahuy14 <trgiahuy14@gmail.com>
2025-12-02 09:21:56 +0700

    feat(tasks): implement TaskController with resource methods and update routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,4 +18,14 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
+    Route::get('/create', [TaskController::class, 'create'])->name('tasks.create');
+    Route::post('/', [TaskController::class, 'store'])->name('tasks.store');
+    Route::get('/{task}', [TaskController::class, 'show'])->name('tasks.show');
+    Route::get('/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+    Route::put('/{task}', [TaskController::class, 'update'])->name('tasks.update');
+    Route::delete('/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+});
